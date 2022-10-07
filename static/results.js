0c834a1a5ef7e364d6a27b601895491770ca8bb4
18OJohnston.acc@lwetb.ie <18OJohnston.acc@lwetb.ie>
2022-10-07 12:06:18 +0100

    got results worked

--- static/results.js
+++ static/results.js
@@ -1,6 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-	var restable = document.getElementById("results");
-	
+	let restable = document.getElementById("results");
+	fetch(url = "/api/guesses", {
+		method: 'GET'
+	}).then(async function(response) {
+		var resp = await response.json();
+		console.log(resp)
+		for (var i = 0; i<resp.length; i ++) {
+			var item = resp[i]
+			
+			var row = restable.insertRow(-1);
+			var namecell = row.insertCell(0);
+			var guessescell = row.insertCell(1);
+			var compcell = row.insertCell(2);
+			namecell.innerHTML = item["user"]["name"];
+			guessescell.innerHTML = item["numguesses"];
+			compcell.innerHTML = Boolean(item["completed"]);
+		}
+		
+	})
 })
 
 
